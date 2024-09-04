aa7cbf47f6be455896efbe36b658412dee7b6b85
M Hammad Afzal <137163382+MHammad33@users.noreply.github.com>
2024-09-04 16:37:46 +0500

    Add express-async-error library

--- backend/src/db/connectDb.ts
+++ backend/src/db/connectDb.ts
@@ -3,7 +3,9 @@ import logger from "../utils/logger";
 
 const connectDB = async (mongoUri?: string) => {
   try {
-    const connection = await mongoose.connect(mongoUri || "");
+    const connection = await mongoose.connect(mongoUri || "", {
+      serverSelectionTimeoutMS: 5000
+    });
     logger.info("Connected to Database", connection.connection.name);
   } catch (error) {
     console.log("Error connecting to Database", error);
