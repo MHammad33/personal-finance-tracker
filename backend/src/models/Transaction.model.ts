4d5d9347eae3b0fae058a9608da5a52f37fc2f3b
M Hammad Afzal <137163382+MHammad33@users.noreply.github.com>
2024-09-11 15:12:47 +0500

    Create Transaction.model.ts

---
+++ backend/src/models/Transaction.model.ts
@@ -0,0 +1,56 @@
+import mongoose from "mongoose";
+
+export interface Transaction extends Document {
+  userId: mongoose.Types.ObjectId;
+  amount: number;
+  type: "income" | "expense";
+  category: string;
+  date: Date;
+}
+
+const transactionSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User"
+    },
+    amount: {
+      type: Number,
+      required: true,
+      min: 0
+    },
+    type: {
+      type: String,
+      enum: ["income", "expense"],
+      required: true
+    },
+    category: {
+      type: String,
+      required: true
+    },
+    date: {
+      type: Date,
+      default: Date.now
+    }
+  },
+  {
+    timestamps: true
+  }
+);
+
+transactionSchema.set("toJSON", {
+  transform: (document, returnedObj) => {
+    returnedObj.id = returnedObj._id.toString();
+    delete returnedObj._id;
+    delete returnedObj.__v;
+    delete returnedObj.password;
+  }
+});
+
+const Transaction = mongoose.model<Transaction>(
+  "Transaction",
+  transactionSchema
+);
+
+module.exports = Transaction;
