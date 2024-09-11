b17f1648836cddfbac966eedcd5552be5ca4ea3b
M Hammad Afzal <137163382+MHammad33@users.noreply.github.com>
2024-09-11 15:58:24 +0500

    Add Add Transaction test

--- backend/src/controllers/transactions.controller.ts
+++ backend/src/controllers/transactions.controller.ts
@@ -5,7 +5,7 @@ const fetchAllTransactions = async (req: Request, res: Response) => {};
 
 const createNewTransaction = async (req: Request, res: Response) => {
   const { amount, type, category, date } = req.body;
-  const userId = (req as any).userId;
+  const userId = req.user;
   const newTransaction = new Transaction({
     userId,
     amount,
---
+++ backend/src/tests/transactions.test.ts
@@ -0,0 +1,68 @@
+import test, { after, afterEach, before, describe } from "node:test";
+import connectDB from "../db/connectDb";
+import config from "../utils/config";
+import mongoose from "mongoose";
+import User from "../models/User.model";
+import Transaction from "../models/Transaction.model";
+import app from "../app";
+import supertest from "supertest";
+
+const baseUrl = "/api/v1";
+const api = supertest(app);
+
+const testUser = {
+  username: `testuser`,
+  email: `testuser@example.com`,
+  password: "password123"
+};
+
+before(async () => {
+  await connectDB(config.MONGODB_URI);
+});
+
+after(async () => {
+  await mongoose.connection.close();
+});
+
+afterEach(async () => {
+  await User.deleteMany({});
+  await Transaction.deleteMany({});
+  if (mongoose.connection.db) {
+    await mongoose.connection.db.dropDatabase();
+  }
+});
+
+describe("Transaction API", () => {
+  let token: string;
+
+  before(async () => {
+    await connectDB(config.MONGODB_URI);
+
+    await api.post(`${baseUrl}/auth/register`).send(testUser).expect(201);
+    const loginResponse = await api
+      .post(`${baseUrl}/auth/login`)
+      .send({
+        email: testUser.email,
+        password: testUser.password
+      })
+      .expect(200);
+
+    token = loginResponse.body.token;
+  });
+
+  describe("POST /api/v1/transactions", () => {
+    test("should add a new transaction", async () => {
+      const transactionData = {
+        amount: 100,
+        type: "income",
+        category: "Salary"
+      };
+
+      const response = await api
+        .post(`${baseUrl}/transactions`)
+        .set("Authorization", `Bearer ${token}`)
+        .send(transactionData)
+        .expect(201);
+    });
+  });
+});
