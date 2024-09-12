67fd039548173a7fd43cd1b23cc9f2fb6c269da8
M Hammad Afzal <137163382+MHammad33@users.noreply.github.com>
2024-09-12 17:14:33 +0500

    Change proxy

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
 	server: {
 		proxy: {
 			"/api": {
-				target: "http://localhost:3001",
+				target: "https://personal-finance-tracker-backend-kohl.vercel.app/",
 				changeOrigin: true,
 			},
 		},
