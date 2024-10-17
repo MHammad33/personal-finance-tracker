9a2d3bc3675c348f68c6dfe5510c4569675e09c6
M Hammad Afzal <137163382+MHammad33@users.noreply.github.com>
2024-10-17 05:02:44 +0500

    refactor: enhance type safety in create transaction controller

--- src/controllers/transactions.controller.ts
+++ src/controllers/transactions.controller.ts
@@ -1,12 +1,23 @@
 import { Request, Response } from "express";
 import Transaction from "../models/Transaction.model";
 
+interface TransactionBody {
+  amount: number;
+  type: string;
+  category: string;
+  date: Date;
+  description: string;
+}
+
 const fetchAllTransactions = async (req: Request, res: Response) => {
   const transactions = await Transaction.find({ userId: req.user?.userId });
   res.status(200).json(transactions);
 };
 
-const createNewTransaction = async (req: Request, res: Response) => {
+const createNewTransaction = async (
+  req: Request<{}, {}, TransactionBody>,
+  res: Response
+) => {
   const { amount, type, category, date, description } = req.body;
   const userId = req.user?.userId;
   const newTransaction = new Transaction({
