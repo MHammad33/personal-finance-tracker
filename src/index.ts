f940e25a46dfaf7df9dcebe74fd5658f5aa58259
M Hammad Afzal <137163382+MHammad33@users.noreply.github.com>
2024-10-18 06:14:32 +0500

    Add retry connection for database

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,24 @@ import connectDB from "./db/connectDb";
 import config from "./utils/config";
 import logger from "./utils/logger";
 
+const connectDbWithRetry = async (retries = 5) => {
+  while (retries) {
+    try {
+      await connectDB(config.MONGODB_URI);
+      break;
+    } catch (error) {
+      console.error(`Retrying... (${retries})`, error);
+      retries -= 1;
+      await new Promise(res => setTimeout(res, 5000));
+    }
+  }
+
+  if (!retries) process.exit(1);
+};
+
 const startServer = async () => {
   try {
-    await connectDB(config.MONGODB_URI);
+    await connectDbWithRetry();
     app.listen(config.PORT, () =>
       logger.info("Server listening on port " + config.PORT)
     );
